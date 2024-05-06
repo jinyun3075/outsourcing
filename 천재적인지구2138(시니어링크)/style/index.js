c1c22ad899a2dd967ace8702502abdd9d47aaab8
YunJae Jin <64072136+jinyun3075@users.noreply.github.com>
2024-05-06 20:42:23 +0900

    Delete 천재적인지구2138(시니어링크) directory

--- 천재적인지구2138(시니어링크)/style/index.js
+++
@@ -1,6 +0,0 @@
-function showModal(){
-    let modal = document.getElementById('myModal');
-    if(modal.style.display=="block")
-        modal.style.display = "none";
-    else modal.style.display = "block";
-}
