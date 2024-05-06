85993639b2d048ab20c8f3a40e9b01d05934de40
YunJae Jin <64072136+jinyun3075@users.noreply.github.com>
2024-05-06 20:31:31 +0900

    1차 페이지

---
+++ 천재적인지구2138(시니어링크)/style/index.js
@@ -0,0 +1,6 @@
+function showModal(){
+    let modal = document.getElementById('myModal');
+    if(modal.style.display=="block")
+        modal.style.display = "none";
+    else modal.style.display = "block";
+}
