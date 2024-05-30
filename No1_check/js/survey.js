1239e2a51810cccc9e71026e9cca5bce7c5760cc
YunJae Jin <64072136+jinyun3075@users.noreply.github.com>
2024-05-30 14:29:15 +0900

    설문 결과 추가

--- No1_check/js/survey.js
+++ No1_check/js/survey.js
@@ -1,5 +1,4 @@
 let currentTab = 0;
-
 function showTab(index) {
     const tabs = document.querySelectorAll('#tabContent > div');
     const tabMenuItems = document.querySelectorAll('#tabMenu li');
@@ -10,9 +9,19 @@ function showTab(index) {
 
     currentTab = index;
 
-    document.querySelector('.btnWrap .gray').style.display = index === 0 ? 'none' : 'inline-block';
-    document.querySelector('.btnWrap .pink').style.display = index === tabs.length - 1 ? 'none' : 'inline-block';
-    document.querySelector('#submitBtn').style.display = index === tabs.length - 1 ? 'inline-block' : 'none';
+    if(currentTab === 99){
+        document.querySelector('.result_box').classList.toggle('hidden')
+        document.querySelector('.result_box').classList.toggle('active')
+        document.querySelector('.btnWrap .gray').style.display = 'none';
+        document.querySelector('.btnWrap .pink').style.display = 'none';
+        document.querySelector('#submitBtn').style.display = 'none';
+    }else {
+
+        document.querySelector('.btnWrap .gray').style.display = index === 0 ? 'none' : 'inline-block';
+        document.querySelector('.btnWrap .pink').style.display = index === tabs.length - 1 ? 'none' : 'inline-block';
+        document.querySelector('#submitBtn').style.display = index === tabs.length - 1 ? 'inline-block' : 'none';
+    }
+
 }
 
 function nextTab() {
@@ -23,24 +32,50 @@ function nextTab() {
     }
 }
 
+function showResult(){
+    let score = 0;
+    let level = '6';
+    const checked = document.querySelectorAll('input[type="radio"]:checked');
+    checked.forEach(d=>score += Number(d.value))
+
+    if(score >= 95){
+        level = '1';
+    }else if(score >= 75){
+        level = '2';
+    }else if(score >= 60){
+        level = '3';
+    }else if(score >= 51){
+        level = '4';
+    }else if(score >= 45){
+        level = '5';
+    }
+    document.querySelector('#rs_score').textContent = score + '점';
+    document.querySelector('#rs_level').textContent = level == 6? '인지지원등급('+ level + '등급)': level +'등급';
+    document.querySelector('#level'+level).classList.toggle('hidden')
+    showTab(99);
+}
+
 function prevTab() {
     showTab(currentTab - 1);
 }
 
 function submitForm() {
-const tabs = document.querySelectorAll('#tabContent > div');
-for (let i = 0; i < tabs.length; i++) {
-    if (!validateTab(i)) {
-        alert("모든 질문에 응답해 주세요.");
-        return;
+     const tabs = document.querySelectorAll('#tabContent > div');
+    for (let i = 0; i < tabs.length; i++) {
+        if (!validateTab(i)) {
+            alert("모든 질문에 응답해 주세요.");
+            return;
+        }
     }
-}
-document.getElementById("surveyForm").submit();
+    // 백엔드 통신 트리거
+    //document.getElementById("surveyForm").submit();
+    showResult()
 }
 
 function validateTab(index) {
     const currentTabInputs = document.querySelectorAll(`#tabContent > div:nth-child(${index + 1}) input`);
     return Array.from(currentTabInputs).some(input => input.checked);
+    return true
 }
 
 document.addEventListener("DOMContentLoaded", function() {
